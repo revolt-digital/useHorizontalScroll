fd32cb8864b74e7265172389dfa4b3cd4ce34779
maitzeth <andre801mz@gmail.com>
2022-05-05 14:02:48 -0300

    Fixing merge conflict

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,32 @@
 import { useRef, useEffect } from "react";
 
-const useHorizontalScroll = () => {
+interface Props {
+  type?: 'scroll' | 'hidden';
+  gap?: number;
+}
+
+const useHorizontalScroll = (props?: Props) => {
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    ref.current?.addEventListener('wheel', (event: WheelEvent) => {
-      event.preventDefault();
-      if (ref.current) {
-        ref.current.scrollLeft += event.deltaY;
-      }
-    });
+    if (ref.current) {
+      ref.current.style.display = 'flex';
+      ref.current.style.overflowX = props?.type ?? 'hidden';
+      ref.current.style.gap = `${props?.gap ?? 0}px`;
+
+      const childrens = Array.from(ref.current.children as HTMLCollectionOf<HTMLElement>);
+
+      childrens.forEach(
+        (children) => (children.style.flex = "0 0 auto")
+      );
+      
+      ref.current.addEventListener("wheel", (event: WheelEvent) => {
+        event.preventDefault();
+        if (ref.current) {
+          ref.current.scrollLeft += event.deltaY;
+        }
+      });
+    }
   }, [ref]);
 
   return { ref };
